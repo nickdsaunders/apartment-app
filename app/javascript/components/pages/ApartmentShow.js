6a3e310500ddb69ad9948900ada48441c6949ecd
nickdsaunders <>
2021-04-08 23:52:43 -0700

    added create and update functionality

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Nav, NavItem, NavLink, Button } from "reactstrap";
 
 class ApartmentShow extends Component {
   render() {
@@ -7,6 +8,7 @@ class ApartmentShow extends Component {
     return (
       <>
         <h1>Apartment Info</h1>
+        <div>{apartment.title}</div>
         <div>Street: {apartment.street}</div>
         <div>City: {apartment.city}</div>
         <div>State: {apartment.state}</div>
@@ -16,6 +18,11 @@ class ApartmentShow extends Component {
         <div>Bedrooms: {apartment.bedrooms}</div>
         <div>Bathrooms: {apartment.bathrooms}</div>
         <div>pets: {apartment.pets}</div>
+        <img className="img-show" src={apartment.img} />
+
+        <NavLink href={`/apartmentedit/${this.props.apartment.id}`}>
+          <Button color="secondary">Edit Listing</Button>
+        </NavLink>
       </>
     );
   }
